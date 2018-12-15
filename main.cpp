2186ab5dfc03a7ee7c2c4bf99aa30af7edbb8bc7
Uros Stojiljkovic <urosss007@gmail.com>
2018-12-15 23:02:13 +0100

    Created interactive menu

--- main.cpp
+++ main.cpp
@@ -1,82 +1,151 @@
 #include "BMPImage.h"
+#include "MazeGraph.h"
 #include <ctime>
+#include <string>
 
+
+void printMainMenu() {
+	cout << "\nChoose one of the following options:\n";
+	cout << "	1. Create a maze\n";
+	cout << "	2. Create a graph from a maze\n";
+	cout << "	3. Solve a maze\n";
+	cout << "	0. Exit\n";
+	cout << "Your choice: ";
+}
+
+void printMazeLoadingMenu() {
+	cout << "\nChoose how to create a maze:\n";
+	cout << "	1. Generate a maze\n";
+	cout << "	2. Read a maze from a bitmap file\n";
+	cout << "	3. Read a maze from standard input\n";
+	cout << "	0. Exit\n";
+	cout << "Your choice: ";
+}
+
+void printSolveMenu() {
+	// to do...
+}
+
+MazeGraph* createGraph(const Maze &m) {
+	clock_t t1, t2;
+	double d;
+	MazeGraph *g;
+	t1 = clock();
+	cout << "\nCreating a graph..." << endl;
+	g = MazeGraph::createGraph(m);
+	t2 = clock();
+	d = (double) (t2 - t1) / CLOCKS_PER_SEC;
+	cout << "Time elapsed: " << d << "s" << endl;
+	return g;
+}
 
 int main() {
 
 	clock_t t1, t2;
 	double d;
-	t1 = clock();
+	//t1 = clock();
 
 	srand(time(nullptr));
 
-	/*BMPImage *i1 = new BMPImage();
+	int choice, w, h, e;
+	string fileName;
+	BMPImage *i = nullptr;
+	MazeGraph *g = nullptr;
+	Maze m;
+	bool loaded = false;
+	while (1) {
+		printMainMenu();
+		cin >> choice;
 
-	ifstream in1("test.bmp", ios_base::binary);
-	if (!in1.is_open()) exit(1);
-	in1 >> *i1;
-	in1.close();
-	//cout << *i1;
+		switch (choice) {
+		case 1:
+			loaded = false;
+			delete g;
+			g = nullptr;
+			delete i;
+			i = nullptr;
+			while (!loaded) {
+				printMazeLoadingMenu();
+				cin >> choice;
+				switch (choice) {
+				case 1:
+					cout << "Enter maze dimensions: ";
+					cin >> w >> h;
+					cout << "Enter number of exits: ";
+					cin >> e;
+					cout << "\nGenerating a maze..." << endl;
+					t1 = clock();
+					m = Maze::generateMaze(w, h, e);
+					t2 = clock();
+					d = (double) (t2 - t1) / CLOCKS_PER_SEC;
+					cout << "Time elapsed: " << d << "s" << endl;
+					i = new BMPImage(m);
+					i->exportAsBMP("maze.bmp");
+					loaded = true;
+					break;
+				case 2:
+					cout << "Enter filename: ";
+					cin >> fileName;
+					cout << "\nLoading a maze..." << endl;
+					t1 = clock();
+					i = new BMPImage();
+					i->importFromBMP(fileName);
+					m = i->convertToMaze();
+					t2 = clock();
+					d = (double) (t2 - t1) / CLOCKS_PER_SEC;
+					cout << "Time elapsed: " << d << "s" << endl;
+					loaded = true;
+					break;
+				case 3:
+					cout << "Enter maze dimensions and maze data (1 for walls and 0 for path): ";
+					cin >> m;
+					i = new BMPImage(m);
+					i->exportAsBMP("maze.bmp");
+					loaded = true;
+					break;
+				case 0:
+					exit(0);
+				default:
+					cout << "Invalid choice, try again.\n";
+					break;
+				}
+			}
+			break;
+		case 2:
+			if (!loaded) {
+				cout << "You need to load a maze first.\n";
+				break;
+			}
+			delete g;
+			g = createGraph(m);
+			break;
+		case 3:
+			if (!loaded) {
+				cout << "You need to load a maze first.\n";
+				break;
+			}
+			if (!g)
+				g = createGraph(m);
 
-	ofstream out1("test_cpy.bmp", ios_base::binary);
-	if (!out1.is_open()) exit(2);
-	out1 << *i1;
-	out1.close();*/
+			t1 = clock();
+			cout << "\nSolving..." << endl;
+			m.setPathColor(g->solveBFS());
+			t2 = clock();
+			d = (double) (t2 - t1) / CLOCKS_PER_SEC;
+			cout << "Time elapsed: " << d << "s" << endl;
 
-	/*BMPImage *i2 = new BMPImage(*i1);
-	BMPImage *i3 = new BMPImage(), *i4 = new BMPImage();
-	*i3 = *i1;
-	*i4 = BMPImage(*i1);
+			i = new BMPImage(m);
 
-	ofstream out2("test_cpy2.bmp", ios_base::binary);
-	if (!out2.is_open()) exit(2);
-	out2 << *i2;
-	out2.close();
+			cout << "Enter a name of solution output file: ";
+			cin >> fileName;
+			i->exportAsBMP(fileName);
+			break;
+		case 0:
+			exit(0);
+		default:
+			cout << "Invalid choice, try again.\n";
+			break;
+		}
+	}
 
-	ofstream out3("test_cpy3.bmp", ios_base::binary);
-	if (!out3.is_open()) exit(2);
-	out3 << *i3;
-	out3.close();
-
-	ofstream out4("test_cpy4.bmp", ios_base::binary);
-	if (!out4.is_open()) exit(2);
-	out4 << *i4;
-	out4.close();*/
-
-	/*Maze m;
-	cin >> m;
-	cout << "\nM1:\n" << m << endl;;
-	char *data = m.convertToPixelData();
-
-	BMPImage *test = new BMPImage(m.getWidth(), m.getHeight(), data);
-	ofstream out("out.bmp", ios_base::binary);
-	if (!out.is_open()) exit(2);
-	out << *test;
-	out.close();
-
-	Maze m2 = test->convertToMaze();
-	cout << "\nM2:\n" << m2 << '\n';*/
-
-	/*BMPImage *i = new BMPImage();
-
-	ifstream in("braid200.bmp", ios_base::binary);
-	if (!in.is_open()) exit(2);
-	in >> *i;
-	in.close(); 
-
-	Maze m = i->convertToMaze();
-	//cout << m;*/
-
-	Maze m = Maze::generateMaze(501, 501, 10);
-	//cout << m;
-
-	BMPImage *test = new BMPImage(m.getWidth(), m.getHeight(), m.convertToPixelData());
-	ofstream out("out.bmp", ios_base::binary);
-	if (!out.is_open()) exit(2);
-	out << *test;
-	out.close();
-
-	t2 = clock();
-	d = (double) (t2 - t1) / CLOCKS_PER_SEC * 1000;
-	cout << "Time elapsed: " << d << "ms" << endl;
 }
